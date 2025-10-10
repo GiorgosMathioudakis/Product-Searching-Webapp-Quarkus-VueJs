8685f68e20f12abf286b0242e8c520b158702eae
Giorgos Mathioudakis <mathioudakes@gmail.com>
2025-10-10 17:17:42 +0300

    vuetify on frontend

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export default createVuetify({
+  components,
+  directives,
+  // Add global configuration here
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
