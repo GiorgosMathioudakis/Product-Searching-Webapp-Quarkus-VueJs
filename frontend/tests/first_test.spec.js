ec3ebcf3bca7baa0cbd0798cb5a02ca3590622a1
Giorgos Mathioudakis <92247285+GiorgosMathioudakis@users.noreply.github.com>
2025-11-17 16:58:50 +0200

    update product test

--- frontend/tests/first_test.spec.js
+++ frontend/tests/first_test.spec.js
@@ -50,3 +50,25 @@ test( 'create product ', async({ page }) => {
   await expect(page.getByRole('cell', { name: 'T-Shirt', exact: true }).first()).toBeVisible();
 
 });
+
+test( 'update product ', async({ page }) => {
+
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('row', { name: 'T-Shirt $20.00 GR-1 Carhartt' }).getByLabel('Edit product').click();
+
+  await page.getByRole('textbox', { name: 'Name Name' }).fill('Old T-Shirt');
+
+  await page.getByRole('textbox', { name: 'SKU SKU' }).fill('GR-2');
+
+  await page.getByRole('textbox', { name: 'Description Description' }).fill('Old Carhartt T-Shirt');
+
+  await page.getByRole('spinbutton', { name: 'Price Price' }).fill('15');
+
+  await page.getByRole('button', { name: 'Save' }).click();
+
+  await expect(page.getByText('Loading products...')).toBeHidden({ timeout: 10000 });
+
+  await expect(page.getByRole('cell', { name: 'Old T-Shirt', exact: true }).first()).toBeVisible();
+
+});
