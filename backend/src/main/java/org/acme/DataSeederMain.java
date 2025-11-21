6847446dfb81d9a87d1ed01432ba41e41a825f2b
Giorgos Mathioudakis <92247285+GiorgosMathioudakis@users.noreply.github.com>
2025-11-21 15:46:40 +0200

    feat: data seeder
    run :
     ./mvnw quarkus:dev "-Dquarkus.package.main-class=db-seeder"

---
+++ backend/src/main/java/org/acme/DataSeederMain.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
+import org.acme.Seeders.DataSeeder;
+
+@QuarkusMain(name = "db-seeder")
+public class DataSeederMain implements QuarkusApplication {
+
+    @Inject
+    DataSeeder dataSeeder;
+
+    @Override
+    public int run(String... args) throws Exception {
+
+        System.out.println("Starting DataSeederMain");
+
+        dataSeeder.loadTenMillionData();
+
+        return 0;
+    }
+
+
+}
---
+++ backend/src/main/java/org/acme/Service/DataSeeder.java
@@ -0,0 +1,54 @@
+
+package org.acme.Seeders;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.Model.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
+import org.hibernate.Transaction;
+
+import java.math.BigDecimal;
+
+@ApplicationScoped
+public class DataSeeder {
+
+    @Inject
+    SessionFactory sessionFactory;
+
+    public void loadTenMillionData()
+    {
+        System.out.println("Starting bulk load...");
+        long start = System.currentTimeMillis();
+
+        try(StatelessSession session = sessionFactory.openStatelessSession()) {
+            Transaction tx = session.beginTransaction();
+
+            for (int i = 0; i < 10_000_000; i++) {
+                Product p = new Product();
+                p.name = "Product " + i;
+                p.description = "Description for product " + i;
+                p.sku = "SKU " + i;
+                p.price = new BigDecimal(i / 1000);
+
+                session.insert(p);
+
+                if(i % 1000 == 0){
+                    tx.commit();
+                    tx = session.beginTransaction();
+                    System.out.println("Inserted " + i + " products");
+                }
+
+            }
+
+            tx.commit();
+
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Finished in " + (end - start) / 1000 + " seconds.");
+
+    }
+
+}
